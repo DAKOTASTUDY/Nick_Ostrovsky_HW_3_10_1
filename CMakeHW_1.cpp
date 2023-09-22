5b42870f7352ffeefe09c76f5deb709b6a4f9821
Nick Ostrovsky <legatus357@gmail.com>
2023-09-21 20:41:07 -0700

    not so fast

--- CMakeHW_1.cpp
+++ CMakeHW_1.cpp
@@ -4,7 +4,7 @@ int main(int argc, char** argv)
 	std::string alfa;
 	std::cout << "\n Please, enter your name:" << std::endl;
 	std::cin >> alfa;
-
+	std::cout << "\n Hello, " << alfa << std::endl;
 	
 	
 	return 0;
