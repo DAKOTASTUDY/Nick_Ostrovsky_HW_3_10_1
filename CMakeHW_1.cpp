95ddf2d84d6ba736ed1776acbf8797a098024e86
Nick Ostrovsky <legatus357@gmail.com>
2023-09-21 20:28:10 -0700

    Add project files.

---
+++ CMakeHW_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(int argc, char** argv)
+{
+	std::string alfa;
+	std::cout << "\n Please, enter your name:" << std::endl;
+	std::cin >> alfa;
+
+	std::cout << "\n Hello, " << alfa << std::endl;
+	
+	return 0;
+}
